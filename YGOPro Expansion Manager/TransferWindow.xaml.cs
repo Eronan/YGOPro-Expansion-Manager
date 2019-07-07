368dcc15b065f2389fafa0737769feae8dbab885
Eronan <animeftw@live.co.uk>
2019-07-07 12:25:53 +1000

    Created New Command

--- YGOPro Expansion Manager/TransferWindow.xaml.cs
+++ YGOPro Expansion Manager/TransferWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Data.SQLite;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WinForms = System.Windows.Forms;
 
 namespace YGOPro_Expansion_Manager
 {
@@ -24,7 +27,48 @@ namespace YGOPro_Expansion_Manager
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Command_Create_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //Use Winforms' SaveFile Dialog
+            using (WinForms.SaveFileDialog saveDialog = new WinForms.SaveFileDialog())
+            {
+                if (saveDialog.ShowDialog() == WinForms.DialogResult.OK)
+                {
+                    string filePath = saveDialog.FileName;
+
+                    SQLiteConnection.CreateFile(filePath);
+                    using (SQLiteConnection sqlConn = new SQLiteConnection("Data Source=" + filePath + ";"))
+                    {
+                        sqlConn.Open();
+                        string execSql_Data = "CREATE TABLE 'datas' ('id' integer,'ot' integer, 'alias' integer," +
+                            "'setcode' integer, 'type' integer, 'atk' integer, 'def' integer, 'level' integer," +
+                            "'race' integer, 'attribute' integer, 'category' integer, PRIMARY KEY('id'))";
+                        string execSql_Text = "CREATE TABLE 'texts'('id' integer, 'name' text, 'desc' text," +
+                            "'str1' text, 'str2' text, 'str3' text, 'str4' text, 'str5' text, 'str6' text," +
+                            "'str7' text, 'str8' text, 'str9' text, 'str10' text, 'str11' text, 'str12' text," +
+                            "'str13' text, 'str14' text, 'str15' text, 'str16' text, PRIMARY KEY('id'))";
+
+                        SQLiteCommand sqlCmd_Data = new SQLiteCommand(execSql_Data, sqlConn);
+                        sqlCmd_Data.ExecuteNonQuery();
+
+                        SQLiteCommand sqlCmd_Text = new SQLiteCommand(execSql_Text, sqlConn);
+                        sqlCmd_Text.ExecuteNonQuery();
+
+                        //Dispose
+                        sqlCmd_Data.Dispose();
+                        sqlCmd_Text.Dispose();
+                        sqlConn.Close();
+                    }
+                }
+            }
+        }
+
+        private void Command_Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Command_Save_Executed(object sender, ExecutedRoutedEventArgs e)
         {
 
         }
